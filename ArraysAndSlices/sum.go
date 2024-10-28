02e23b5d8c320a3500948dfeeaec286ab9a0573c
Али Шыхыев <lobiritus@gmail.com>
2024-10-28 23:30:44 +0300

    Refactored ArraysAndSlices

--- ArraysAndSlices/sum.go
+++ ArraysAndSlices/sum.go
@@ -1,9 +1,38 @@
 package ArraysAndSlices
 
-func Sum(arr []int) int {
+func SumSlice(slice []int) int {
+	var result int
+	for _, v := range slice {
+		result += v
+	}
+	return result
+}
+
+func SumArray(arr [5]int) int {
 	var result int
 	for _, v := range arr {
 		result += v
+	}
+	return result
+}
+
+func SumAll(slice ...[]int) []int {
+	var result []int
+	for _, numbers := range slice {
+		result = append(result, SumSlice(numbers))
+	}
+	return result
+}
+
+func SumAllTails(slice ...[]int) []int {
+	var result []int
+	for _, numbers := range slice {
+
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
+			result = append(result, SumSlice(numbers[1:]))
+		}
 	}
 	return result
 }
