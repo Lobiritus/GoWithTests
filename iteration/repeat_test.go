f0e8ab2e19f86ec8b1b6defd2f1e57fc683aa32c
Али Шыхыев <lobiritus@gmail.com>
2024-10-28 21:26:41 +0300

    Added iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,35 @@
+package iteration
+
+import (
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	t.Run("Repeat symbols", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("Repeat text", func(t *testing.T) {
+		repeated := Repeat("text", 3)
+		expected := "texttexttext"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func assertCorrectMessage(t testing.TB, repeated, expected string) {
+	t.Helper()
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
