b7bb4d236a5cdc4704306e8842b1989a8de00e8d
Ali Shykhyev <aa@rossi.ru>
2024-10-29 12:04:28 +0300

    Added PointersAndErrors

--- StructuresMethodsAndInterface/perimeter_test.go
+++ StructuresMethodsAndInterface/perimeter_test.go
@@ -15,19 +15,22 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	areaTest := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Height: 9.0, Width: 5.0}, want: 45.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36},
+		{name: "Rectangle", shape: Rectangle{Height: 9.0, Width: 5.0}, hasArea: 45.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36},
 	}
 
 	for _, tt := range areaTest {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 }
