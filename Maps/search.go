a6f56b56a20d7c78795b995bc25a78cfef274f7c
Ali Shykhyev <aa@rossi.ru>
2024-10-29 17:15:03 +0300

    Refactored maps

--- Maps/search.go
+++ Maps/search.go
@@ -1,10 +1,17 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(key string) (string, error) {
 	val, ok := d[key]
@@ -14,6 +21,15 @@ func (d Dictionary) Search(key string) (string, error) {
 	return val, nil
 }
 
-func (d Dictionary) Add(key, value string) {
-	d[key] = value
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
